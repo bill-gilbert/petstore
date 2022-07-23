df47ddcd0a174bee93c151ec5954f6a3ce2c2b9e
Pavel Glukhov <pvglukhov@gmail.com>
2022-07-24 01:38:10 +0300

    [master] added new files

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '../components/Main'
+import Form from '../components/Form'
 
 Vue.use(Router)
 
@@ -8,8 +9,15 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'iMain',
+      component: Main,
+      props: true
+    },
+    {
+      path: '/',
+      name: 'Form',
+      component: Form,
+      props: true
     }
   ]
 })
